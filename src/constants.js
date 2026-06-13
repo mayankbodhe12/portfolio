7b5d8783e83db023cba0b98f5372cca794a234b1
Mayank Bodhe <mayankbodhe624@gmail.com>
2026-06-13 16:43:40 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -74,40 +74,6 @@ export const experiences = [
     desc: "As a Java Development Intern, I have the opportunity to work on various projects and tasks, develop robust and scalable applications, and contribute to the design and implementation of software solutions.",
     skills: ["java"],
   },
-// {
-//   id: 1,
-//   img: agcLogo,
-//   role: "Fullstack Engineer",
-//   company: "Agumentik Group of Companies",
-//   date: "July 2023 - March 2024",
-//   desc: "Contributed to innovative projects as a Fullstack Engineer, leading both frontend and backend development using technologies such as HTML, CSS, JavaScript, PHP, SQL, Bootstrap, and ReactJS. Worked closely with the team to deliver responsive, high-performance web applications and improve user experience through seamless integration of various technologies.",
-//   skills: [
-//     "ReactJS",
-//     // "Redux",
-//     "JavaScript",
-//     // "Tailwind CSS",
-//     "HTML",
-//     "CSS",
-//     "SQL",
-//   ],
-// },
-//   {
-//     id: 2,
-//     img: newtonschoolLogo,
-//     role: "Frontend Intern",
-//     company: "Newton School",
-//     date: "September 2021 - August 2022",
-//     desc: "Worked as a Frontend Developer Intern, designing and implementing scalable UI components and responsive websites using HTML, CSS, JavaScript, Bootstrap, and Material UI. Collaborated with the design team to translate wireframes and prototypes from Figma into interactive, user-friendly web pages.",
-//     skills: [
-//       "HTML",
-//       "CSS",
-//       "Javascript",
-//       // "Bootstrap",
-//       // "Figma",
-//       // "Material UI",
-//     ],
-//   },
-];
 
 export const education = [
   {
